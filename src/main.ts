a1d025a45783a2c965cc633b21602dc4f1e80b20
kidmortal <kidmortal@gmail.com>
2023-12-26 10:30:25 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { BlockChainModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(BlockChainModule);
+
+  app.enableCors();
+
   await app.listen(port || 3000);
 }
 bootstrap();
