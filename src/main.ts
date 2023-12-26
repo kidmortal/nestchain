699f8503f3fe83bd63679d54424a39882f3df845
kidmortal <kidmortal@gmail.com>
2023-12-26 09:34:44 -0300

    add port from env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { BlockChainModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT;
   const app = await NestFactory.create(BlockChainModule);
-  await app.listen(3000);
+  await app.listen(port || 3000);
 }
 bootstrap();
